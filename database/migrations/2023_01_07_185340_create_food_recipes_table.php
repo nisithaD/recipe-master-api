a0d4a90eb8a65947a489942c9efc397c2a025bcb
Nisitha Dayarathna <nisitha56190@gmail.com>
2023-01-08 22:49:44 +0530

    Seeder added for seed recipes

--- database/migrations/2023_01_07_185340_create_food_recipes_table.php
+++ database/migrations/2023_01_07_185340_create_food_recipes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('food_recipes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('Ingredients');
+            $table->string('ingredients');
             $table->longText('description');
             $table->string('image');
             $table->float('calorie_count');
